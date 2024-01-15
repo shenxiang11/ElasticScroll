6b4171df52b744119c628d1be7b24fa99cb1dfbf
shenxiang <shenxiang@foursmile001.com>
2024-01-15 11:21:00 +0800

    Initial Commit

---
+++ ElasticScroll/ElasticScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElasticScrollApp.swift
+//  ElasticScroll
+//
+//  Created by FS on 2024/1/15.
+//
+
+import SwiftUI
+
+@main
+struct ElasticScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
